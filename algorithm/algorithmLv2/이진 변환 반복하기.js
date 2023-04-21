8d89cb874abefa0eeb1121781171662d5aa049f0
jhin3283 <hj3283@gmail.com>
2023-04-21 10:25:45 +0900

    STUDY: Algorithm

---
+++ algorithm/algorithmLv2/이진 변환 반복하기.js
@@ -0,0 +1,20 @@
+function solution(s) {
+    var answer = [];
+    let delNum = 0;
+    let loop = 0;
+    while (s.length > 1){
+        for(let i=0; i<s.length; i++){
+            if(s[i] === "0"){
+                delNum++
+            }
+        }
+        s = s.replace(/0/g, '');
+        let length = s.length
+        let binary = length.toString(2);
+        s= binary
+        loop++
+    }
+    answer.push(loop,delNum)
+    return answer;
+}
+// toString(2)로 이진법 변환이 핵심
