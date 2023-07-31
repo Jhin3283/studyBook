04da3bda79e32d9ecb4a0898dd4470475d45a3ee
Jhin3283 <hj3283@gmail.com>
2023-07-31 16:59:33 +0900

    [Temp] algorithm

--- algorithm/test.js
+++ algorithm/test.js
@@ -1,24 +1,113 @@
-function solution(n, k, enemy) {
-  let [left, right] = [0, enemy.length];
-  let mid = Math.floor((left + right) / 2);
-  while (left <= right) {
-    const curSlice = enemy.slice(0, mid).sort((a, b) => b - a);
-    let noDie = k;
-    const curEnemy = curSlice.reduce((acc, cur) => {
-      if (noDie > 0) {
-        noDie--;
-        return acc;
-      }
-      return acc + cur;
-    }, 0);
-    if (n - curEnemy >= 0 && noDie >= 0) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
-    mid = Math.floor((left + right) / 2);
-  }
-  return left - 1;
-}
+// function solution(board) {
+//   let map = [];
+//   board.map((el) => {
+//     map.push(el.split(""));
+//   });
+//   let xAxis = [-1, 1, 0, 0];
+//   let yAxis = [0, 0, 1, -1];
+//   let S;
+//   for (let i = 0; i < map.length; i++) {
+//     for (let j = 0; j < map[0].length; j++) {
+//       if (map[i][j] === "R") {
+//         S = [i, j];
+//         map[i][j] = "0";
+//       }
+//     }
+//   }
+//   let stack = [[...S, 0]];
+//   while (stack.length) {
+//     const size = stack.length;
+//     for (let j = 0; j < size; j++) {
+//       const [x, y, cnt] = stack.shift();
+//       for (let i = 0; i < 4; i++) {
+//         let [nx, ny] = [x + xAxis[i], y + yAxis[i]];
+//         while (
+//           0 <= nx &&
+//           nx < map.length &&
+//           0 <= ny &&
+//           ny < map[0].length &&
+//           map[nx][ny] !== "D"
+//         ) {
+//           [nx, ny] = [x + xAxis[i], y + yAxis[i]];
+//         }
+//         [nx, ny] = [x - xAxis[i], y - yAxis[i]];
+//         console.log(nx, ny);
+//         if (map[nx][ny] === "G") return cnt + 1;
+//         else if (map[nx][ny] !== "0") {
+//           map[nx][ny] = 0;
+//           stack.push([nx, ny, cnt + 1]);
+//         }
+//       }
+//     }
+//     cnt++;
+//   }
+//   return -1;
+// }
+console.log(solution(["...D..R", ".D.G...", "....D.D", "D....D.", "..D...."]));
+function solution(board) {
+  var answer = 0;
+  board = board.map((items) => items.split(""));
 
-console.log(solution(2, 2, [3, 3, 3, 3]));
+  const q = [];
+  const n = board.length; // 가로 길이
+  const m = board[0].length; // 세로 길이
+  const dx = [-1, 1, 0, 0]; // 상하좌우 방향
+  const dy = [0, 0, -1, 1];
+
+  board.forEach((items, i) => {
+    items.forEach((item, j) => {
+      if (item === "R") q.push([i, j]); // 시작 위치
+    });
+  });
+
+  // 1) 시작 위치를 다시 방문하지 않게 'O' 표시
+  board[q[0][0]][q[0][1]] = "O";
+
+  // 2) q의 길이 만큼 반복
+  while (q.length) {
+    // 3) 횟수(answer)를 카운트하기 위해 현재 q의 길이를 고정시킨다.
+    const size = q.length;
+
+    // 4) 고정시킨 길이만큼 반복한다.
+    for (let i = 0; i < size; i++) {
+      const [x, y] = q.shift();
+
+      // 5) 상하좌우 한번씩 확인
+      for (let j = 0; j < 4; j++) {
+        // 6) 다음 이동 위치
+        let nx = x + dx[j];
+        let ny = y + dy[j];
+
+        // 7) 게임판 범위와 벽(D)를 만나지 않을 경우만 미끄러진다.
+        while (
+          nx >= 0 &&
+          nx < n &&
+          ny >= 0 &&
+          ny < m &&
+          board[nx][ny] !== "D"
+        ) {
+          nx += dx[j];
+          ny += dy[j];
+        }
+
+        // 8) 현재 위치로 변경한다.
+        nx -= dx[j];
+        ny -= dy[j];
+        console.log(nx, ny);
+        // 9) 현재 위치가 도착(G) 지점이면 횟수(answer)를 1증가 후 반환한다.
+        if (board[nx][ny] === "G") return answer + 1;
+
+        // 10) 한번이라도 방문한적이 없을 경우만
+        if (board[nx][ny] !== "O") {
+          // 11) 방문 표시(O) 후 q에 담는다.
+          board[nx][ny] = "O";
+          q.push([nx, ny]);
+        }
+      }
+      console.log(q);
+    }
+    answer++;
+  }
+
+  return -1;
+}
